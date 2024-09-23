75f296c23fb105113ade6b448b79c00ed121cfa9
Nguyen Anh Quan <nanhquan143@gmail.com>
2024-09-23 23:57:53 +0700

    first commit 09/23/2023

---
+++ server/index.js
@@ -0,0 +1,36 @@
+//express dotenv cors cookie-parser mongoose bcrypt jsonwebtoken nodemon
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import authRoutes from './routes/AuthRoutes.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(
+    cors({
+        origin: [process.env.ORIGIN],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+});
+
+mongoose.connect(databaseURL)
+    .then(() => console.log('Connected to DB successfully.'))
+    .catch((err) => console.log(err.message));
+
+
