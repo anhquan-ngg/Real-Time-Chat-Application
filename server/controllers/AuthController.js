299bc67361c6d676cdba8e51ecf277fbf437ce51
Nguyen Anh Quan <nanhquan143@gmail.com>
2024-09-30 01:19:10 +0700

    add profile image

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,7 @@
 import User from '../models/UserModel.js';
 import jwt from 'jsonwebtoken';
 import {compare} from 'bcrypt';
+import {renameSync, unlinkSync} from "fs";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
@@ -77,6 +78,101 @@ export const getUserInfo = async (req, res, next) => {
         const userData = await User.findById(req.userId);
         if (!userData) return res.status(404).send("User with the given id not found");
         
+        return res.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+        })
+    } catch (error) {
+        console.log({error});
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+export const updateProfile = async (req, res, next) => {
+    try {
+        const {userId} = req;
+        const {firstName, lastName, color} = req.body;
+        
+        if (!firstName || !lastName || color == undefined){
+            return res.status(400).send("Firstname lastname and color are required");
+        }
+
+        const userData = await User.findByIdAndUpdate(
+            userId, 
+            {
+                firstName,
+                lastName,
+                color,
+                profileSetup: true,
+            },
+            {new: true, runValidators: true}
+        )
+
+        return res.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+        })
+    } catch (error) {
+        console.log({error});
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+export const addProfileImage = async (req, res, next) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send("File is required");
+        }
+
+        const date = Date.now();
+        let fileName = "upload/profiles/" + date + req.file.originalname;
+        renameSync(req.file.path, fileName);
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.userId,
+            {image: fileName},
+            {new: true, runValidators: true},
+        );
+
+        return res.status(200).json({
+            image: updatedUser.image,
+        });
+    } catch (error) {
+        console.log({error});
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+export const removeProfileImage = async (req, res, next) => {
+    try {
+        const {userId} = req;
+        const {firstName, lastName, color} = req.body;
+        
+        if (!firstName || !lastName || color == undefined){
+            return res.status(400).send("Firstname lastname and color are required");
+        }
+
+        const userData = await User.findByIdAndUpdate(
+            userId, 
+            {
+                firstName,
+                lastName,
+                color,
+                profileSetup: true,
+            },
+            {new: true, runValidators: true}
+        )
+
         return res.status(200).json({
             id: userData.id,
             email: userData.email,
