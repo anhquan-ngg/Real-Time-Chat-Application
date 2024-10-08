1f2bf5ca08fb438537b3ba7b2263b9aab313e133
Nguyen Anh Quan <nanhquan143@gmail.com>
2024-10-09 01:39:26 +0700

    add search modal

--- client/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
+++ client/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
@@ -1,6 +1,84 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {FaPlus} from "react-icons/fa";
+import {useState} from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import {Input} from "@/components/ui/input";
+import Lottie from "react-lottie";
+import {animationDefaultOptions} from "@/lib/utils.js";
+
+
 const NewDM = () => {
+    const [openNewContactModal, setOpenNewContactModal] = useState(false);
+    const [searchedContacts, setSearchedContacts] = useState([]);
+
+    const searchContact = async (searchTerm) => {
+    }
+
     return (
-        <div>NewDM</div>
+        <>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <FaPlus
+                            className="text-neutral-400 font-light text-opacity-90
+                            hover:text-neutral-100 cursor-pointer transition-all duration-300"
+                            onClick={() => setOpenNewContactModal(true)}
+                        />
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-[#1c1b1e] border-none mb-2 p-3 text-white">
+                        Select New Contact
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+            <Dialog open={openNewContactModal} onOpenChange={setOpenNewContactModal}>
+                <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
+                    <DialogHeader>
+                        <DialogTitle>Please select a contact</DialogTitle>
+                        <DialogDescription></DialogDescription>
+                    </DialogHeader>
+                    <div>
+                        <Input
+                            placeholder="Search Contacts"
+                            className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+                            onChange={e => searchContact(e.target.value)}
+                        />
+                    </div>
+                    {
+                        searchedContacts.length <= 0 && (
+                            <div
+                                className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center transition-all duration-1000">
+                                <Lottie
+                                    isClickToPauseDisabled={true}
+                                    height={100}
+                                    width={100}
+                                    options={animationDefaultOptions}
+                                />
+                                <div
+                                    className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
+                                    <h3 className="poppins-medium">
+                                        Hi<span className="text-purple-500">!</span> Welcome to
+                                        <span className="text-purple-500"> Amazing </span> Chat App
+                                        <span className="text-purple-500">.</span>
+                                    </h3>
+                                </div>
+                            </div>
+                        )
+                    }
+                </DialogContent>
+            </Dialog>
+
+        </>
     );
 };
 export default NewDM;
--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProfileInfo from "./components/profile-info";
+import NewDM from "./components/new-dm/index.jsx";
 
 const ContactsContainer = () => {
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
         <div className="pt-3">
-            <div className="flex p-5  justify-start items-center gap-2">
+            <div className="flex p-5 justify-start items-center gap-2">
                 <svg
                     id="logo-38"
                     width="78"
@@ -37,6 +38,7 @@ const ContactsContainer = () => {
         <div className="my-5">
             <div className="flex items-center justify-between pr-10">
                 <Title text="Direct Message" />
+                <NewDM/>
             </div>
         </div>
         <div className="my-5">
