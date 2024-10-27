a09e5b72de8e4ce5999dcc3631a50beb777739be
Nguyen Anh Quan <nanhquan143@gmail.com>
2024-10-27 11:18:59 +0700

    final

--- client/src/pages/chat/components/chat-container/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/chat-header/index.jsx
@@ -10,11 +10,10 @@ const ChatHeader = () => {
   return (
     <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between px-20">
       <div className="flex gap-5 items-center w-full justify-between">
-        <div className="flex gap-3 items-center justify-center">
             {
               selectedChatType === "contact"
                   ?
-                  (
+                  (<div className="flex gap-3 items-center justify-center">
                       <div className="w-12 h-12 relative">
                         <Avatar className="h-12 w-12 rounded-full overflow-hidden">
                           {selectedChatData.image ? (
@@ -36,23 +35,22 @@ const ChatHeader = () => {
                           )}
                         </Avatar>
                       </div>
-                      &&
                       <div>
                         {selectedChatData.firstName ? `${selectedChatData.firstName} ${selectedChatData.lastName}` : selectedChatData.email}
                       </div>
+                      </div>
                   )
                   :
-                  (
+                  ( <div className="flex gap-3 items-center justify-center">
                       <div className="w-12 h-12 relative">
                         <div className="flex bg-[#ffffff22] text-lg h-12 w-12 items-center justify-center rounded-full">
                           #
                         </div>
                       </div>
-                      &&
                       <div>{selectedChatData.name}</div>
+                      </div>
                   )
             }
-        </div>
         <div className="flex items-center justify-center gap-5">
           <button
               className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
